0389e565c389dee7037d6923b0bada06c1f7b0c8
Ning <vipning@qq.com>
2021-10-06 20:12:41 +0800

    2.3发布

--- WpfApp1/Properties/AssemblyInfo.cs
+++ WpfApp1/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 //
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
-[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
