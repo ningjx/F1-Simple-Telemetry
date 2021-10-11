496b2846ce8f3b045013e0df30042bf23d6102c0
Ning <vipning@qq.com>
2021-10-11 15:57:01 +0800

    性能优化

--- WpfApp1/DataHandle/ShowDataHandle.cs
+++ WpfApp1/DataHandle/ShowDataHandle.cs
@@ -1,12 +1,9 @@
-﻿using System.Linq;
-using static F1Tools.TypeFactory;
+﻿using static F1Tools.TypeFactory;
 
 namespace F1Tools
 {
     public static class ShowDataHandle
     {
-        private static int PlayerIndex = 0;
-
         public static void F1Handle(F1Instrument f1, dynamic packet)
         {
             if (packet == null)
@@ -30,15 +27,10 @@ namespace F1Tools
 
         private static void Handle19(F1Instrument f1, Codemasters.F1_2019.Packet packet)
         {
-            if (packet.PacketType == Codemasters.F1_2019.PacketType.Participants)
-            {
-                var curPack = packet as Codemasters.F1_2019.ParticipantPacket;
-                PlayerIndex = curPack.FieldParticipantData.ToList().FindIndex(t => !t.IsAiControlled);
-            }
-            else if (packet.PacketType == Codemasters.F1_2019.PacketType.CarTelemetry)
+            if (packet.PacketType == Codemasters.F1_2019.PacketType.CarTelemetry)
             {
                 var curPack = packet as Codemasters.F1_2019.TelemetryPacket;
-                var data = curPack.FieldTelemetryData[PlayerIndex];
+                var data = curPack.FieldTelemetryData[curPack.PlayerCarIndex];
 
                 f1.SetBreak(data.Brake);
                 f1.SetThr(data.Throttle);
@@ -59,15 +51,10 @@ namespace F1Tools
 
         private static void Handle20(F1Instrument f1, Codemasters.F1_2020.Packet packet)
         {
-            if (packet.PacketType == Codemasters.F1_2020.PacketType.Participants)
-            {
-                var curPack = packet as Codemasters.F1_2020.ParticipantPacket;
-                PlayerIndex = curPack.FieldParticipantData.ToList().FindIndex(t => !t.IsAiControlled);
-            }
-            else if (packet.PacketType == Codemasters.F1_2020.PacketType.CarTelemetry)
+            if (packet.PacketType == Codemasters.F1_2020.PacketType.CarTelemetry)
             {
                 var curPack = packet as Codemasters.F1_2020.TelemetryPacket;
-                var data = curPack.FieldTelemetryData[PlayerIndex];
+                var data = curPack.FieldTelemetryData[curPack.PlayerCarIndex];
 
                 f1.SetBreak(data.Brake);
                 f1.SetThr(data.Throttle);
@@ -88,15 +75,10 @@ namespace F1Tools
 
         private static void Handle21(F1Instrument f1, Codemasters.F1_2021.Packet packet)
         {
-            if (packet.PacketType == Codemasters.F1_2021.PacketType.Participants)
-            {
-                var curPack = packet as Codemasters.F1_2021.ParticipantPacket;
-                PlayerIndex = curPack.FieldParticipantData.ToList().FindIndex(t => !t.IsAiControlled);
-            }
-            else if (packet.PacketType == Codemasters.F1_2021.PacketType.CarTelemetry)
+            if (packet.PacketType == Codemasters.F1_2021.PacketType.CarTelemetry)
             {
                 var curPack = packet as Codemasters.F1_2021.TelemetryPacket;
-                var data = curPack.FieldTelemetryData[PlayerIndex];
+                var data = curPack.FieldTelemetryData[curPack.PlayerCarIndex];
 
                 f1.SetBreak(data.Brake);
                 f1.SetThr(data.Throttle);
